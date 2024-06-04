979bb4a3a6731a13b572677dd4320e72385adaa4
Tayson09 <146737035+Tayson09@users.noreply.github.com>
2024-06-04 16:05:00 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+// App.js
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <div className="Text-main">
+          Cadastro de Fornecedores
+        </div>
+        {/* Conteúdo adicional do quadro branco */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
